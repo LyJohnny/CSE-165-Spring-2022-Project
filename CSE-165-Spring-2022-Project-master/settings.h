111fda94bed0be079e89a10295e95ae049980c62
LyJohnny <johnbus.ly@gmail.com>
2022-05-07 21:48:02 -0700

    added cannon and alien colors

---
+++ CSE-165-Spring-2022-Project-master/settings.h
@@ -0,0 +1,11 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <QSize>
+
+QSize const gCannonSize = QSize(100,100);
+int const gBulletSpeed = 5;
+int const gAlienSpeed = 50;
+int const gMaxHealth = 3;
+
+#endif // SETTINGS_H
